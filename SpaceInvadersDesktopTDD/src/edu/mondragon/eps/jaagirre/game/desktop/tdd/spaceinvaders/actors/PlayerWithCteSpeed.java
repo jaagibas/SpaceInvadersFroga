b805cbf76cf148b2099ddcac7ce295129d285b79
umarkiegi <umarkiegi@mondragon.edu>
2012-04-23 13:05:24 +0200

    Creada Clase Jugador Velocidad Constante

---
+++ SpaceInvadersDesktopTDD/src/edu/mondragon/eps/jaagirre/game/desktop/tdd/spaceinvaders/actors/PlayerWithCteSpeed.java
@@ -0,0 +1,14 @@
+package edu.mondragon.eps.jaagirre.game.desktop.tdd.spaceinvaders.actors;
+import java.awt.image.BufferedImage;
+
+
+
+public class PlayerWithCteSpeed extends Player {
+
+	public PlayerWithCteSpeed(BufferedImage image, int x, int y, int xLimit,
+			int yLimit) {
+		super(image, x, y, xLimit, yLimit);
+		// TODO Auto-generated constructor stub
+	}
+
+}
