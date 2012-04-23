1158d1eca366c50dde6c8bf86d3b6b5508d24a7e
umarkiegi <umarkiegi@mondragon.edu>
2012-04-23 12:22:35 +0200

    Modificación funcion GameThread.run(). Centralizamos la interfaz de esta clase hacia la clase Game via las funciones Game.step() y Game.isGameRunning()

--- SpaceInvadersDesktopTDD/src/edu/mondragon/eps/jaagirre/game/desktop/tdd/spaceinvaders/GameThread.java
+++ SpaceInvadersDesktopTDD/src/edu/mondragon/eps/jaagirre/game/desktop/tdd/spaceinvaders/GameThread.java
@@ -11,18 +11,14 @@ public class GameThread  extends Thread{
 		//Canvas tiene que tener una refencia al gameLogic y nada mas de manera que lo sbuffer sean del otro
 	@Override
 	public void run() {
-		running = true;
-		GameLogic gameLogic = this.game.getGameLogic();
-		while( !gameLogic.isGameOver() && !gameLogic.isGameWin() ){
-			game.update();
-			game.getBoard().paint( game.getBoard().getGraphics());
-			try{
-             	sleep(100);
-			}catch(Exception e){}
-		}
-		running = false;
-		 
+        running = true;
+        while( game.isGameRunning() ){
+               game.step();
+        }
+        running = false;
+        
 	}
+	
 	public boolean isRunning() {
 		return running;
 	}	
