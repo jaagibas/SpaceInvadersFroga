be62500be3e5b11c19a6066e30d75f6f5c4a0a77
jaagirre <jaagirre@mondragon.edu>
2012-05-02 11:54:37 +0200

    Agregado Factory para el servicio Intersect de Rectangulos con una implementación AWT.

---
+++ SpaceInvadersDesktopTDD/src/edu/mondragon/eps/jaagirre/game/desktop/tdd/spaceinvaders/utils/RectangleServices.java
@@ -0,0 +1,15 @@
+package edu.mondragon.eps.jaagirre.game.desktop.tdd.spaceinvaders.utils;
+
+public class RectangleServices {
+
+	
+	
+	public RectangleServices(){
+		
+	}
+	
+	public boolean intersects(Square s1 , Square s2){
+		return false;
+	}
+	
+}
